1cdf08dd11570ff26317d00d0555b0742c8ce657
LeeW <cl87654321@gmail.com>
2015-04-11 11:08:23 +0800

    Add confirm dialog and broaden url entry length

--- Downloader_GUI.py
+++ Downloader_GUI.py
@@ -3,6 +3,7 @@ from tkinter import Label
 from tkinter import Button
 from tkinter import Entry
 from tkinter import Frame
+from tkinter.messagebox import askokcancel
 from tkinter.filedialog import askdirectory
 
 from Flickr_Album_Downloader import FlickrAlbumDownloader
@@ -18,21 +19,27 @@ class DownloaderGUI(Frame):
 
     def __create_widgets(self):
         self.url_label = Label(self, text="Album URL: ")
-        self.url_field = Entry(self)
-        self.download_btn = Button(self, text="Confirm", command=self.__download_method)
+        self.url_field = Entry(self, width=70)
+        self.download_btn = Button(self, text="Download", command=self.__download_method)
 
         self.url_label.grid(row=0, column=0)
         self.url_field.grid(row=0, column=1)
-        self.download_btn.grid(row=1, column=1)
+        self.download_btn.grid(row=1, column=0)
 
     def __download_method(self):
-        self.directory = askdirectory(parent=self.master,
-                                      initialdir="./download_img")
+        directory = askdirectory(parent=self.master,
+                                 initialdir=".")
 
-        d = FlickrAlbumDownloader()
-        d.set_URL(self.url_field.get())
-        d.parse_all_imgs()
-        d.download_all_img()
+        if directory:
+            d = FlickrAlbumDownloader()
+            d.set_URL(self.url_field.get())
+            d.parse_all_imgs()
+
+            answer = askokcancel("Download Confirm",
+                                 "There are "+str(d.get_img_num())+" images.")
+            if answer:
+                d.set_export_directory(directory)
+                d.download_all_img()
 
 
 if __name__ == '__main__':

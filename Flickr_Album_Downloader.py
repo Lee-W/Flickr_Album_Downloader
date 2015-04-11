87221571287024f3076be4e0e88110c13a7c0a80
LeeW <cl87654321@gmail.com>
2015-04-11 21:44:11 +0800

    Replace  src with origin url
    * change download img quality

--- Flickr_Album_Downloader.py
+++ Flickr_Album_Downloader.py
@@ -18,12 +18,12 @@ class FlickrAlbumDownloader:
 
     def parse_all_imgs(self):
         raw_html = self.__request_raw_html(self.url)
-        self.__parse_title_and_src(raw_html)
+        self.__parse_title_and_url(raw_html)
 
         self.__parse_other_pages_url(raw_html)
         for url in self.other_pages_url:
             raw_html = self.__request_raw_html(url)
-            self.__parse_title_and_src(raw_html)
+            self.__parse_title_and_url(raw_html)
 
     def __request_raw_html(self, url):
         req = requests.get(url)
@@ -41,17 +41,18 @@ class FlickrAlbumDownloader:
             self.other_pages_url = list()
 
     # TODO: refactor
-    def __parse_title_and_src(self, html):
+    def __parse_title_and_url(self, html):
         m = re.findall("Y.listData = {[\S\s]*try", html)
         listData = json.loads(m[0][13:-8])
         for rows in listData["rows"]:
             for row in rows["row"]:
+                origin_size_url = row["sizes"]["o"]["url"]
                 self.albums.append({"full_name": row["full_name"],
-                                    "src": row["src"],
-                                    "file_extension": self.__match_file_extension(row["src"])})
+                                    "url": origin_size_url,
+                                    "file_extension": self.__match_file_extension(origin_size_url)})
 
-    def __match_file_extension(self, src):
-        m = re.search("\.([a-zA-Z]*)$", src)
+    def __match_file_extension(self, url):
+        m = re.search("\.([a-zA-Z]*)$", url)
         return m.groups()[0]
 
     def set_export_directory(self, path):
@@ -68,7 +69,7 @@ class FlickrAlbumDownloader:
         for img in self.albums:
             print("Download %s" % img["full_name"])
             full_file_name = img["full_name"]+"."+img["file_extension"]
-            self.__download(img["src"], self.path+"/"+full_file_name)
+            self.__download(img["url"], self.path+"/"+full_file_name)
 
     def __download(self, url, path):
         urllib.request.urlretrieve(url, path)

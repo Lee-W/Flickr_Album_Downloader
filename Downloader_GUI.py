ee4ac4423ee56b435586527474d81bc66ab8c26a
LeeW <cl87654321@gmail.com>
2015-09-27 01:23:09 +0800

    Update GUI with new FlickrAlbumDownloader

--- Downloader_GUI.py
+++ Downloader_GUI.py
@@ -3,7 +3,7 @@ from tkinter import Label
 from tkinter import Button
 from tkinter import Entry
 from tkinter import Frame
-from tkinter.messagebox import askokcancel
+# from tkinter.messagebox import askokcancel
 from tkinter.filedialog import askdirectory
 
 from Flickr_Album_Downloader import FlickrAlbumDownloader
@@ -14,48 +14,47 @@ class DownloaderGUI(Frame):
         Frame.__init__(self, master)
         self.master = master
         self.grid(columnspan=2000)
-        self.d = FlickrAlbumDownloader()
 
         self.__create_widgets()
 
     def __create_widgets(self):
+        self.key_label = Label(self, text="API Key")
+        self.key_field = Entry(self, width=70)
+
+        self.secret_label = Label(self, text="API Secret")
+        self.secret_field = Entry(self, width=70)
+
         self.url_label = Label(self, text="Album URL: ")
         self.url_field = Entry(self, width=70)
+
         self.download_btn = Button(self, text="Download", command=self.__download_method)
         self.msg_label = Label(self)
 
-        self.url_label.grid(row=0, column=0)
-        self.url_field.grid(row=0, column=1)
-        self.download_btn.grid(row=1, column=0)
+        self.key_label.grid(row=0, column=0)
+        self.key_field.grid(row=0, column=1)
+        self.secret_label.grid(row=1, column=0)
+        self.secret_field.grid(row=1, column=1)
+        self.url_label.grid(row=2, column=0)
+        self.url_field.grid(row=2, column=1)
+        self.download_btn.grid(row=3, column=0)
 
     def __download_method(self):
-        self.msg_label.grid_remove()
+        downloader = FlickrAlbumDownloader(self.key_field.get(), self.secret_field.get())
+        self.msg_label.text = ""
 
         directory = askdirectory(parent=self.master,
                                  initialdir=".")
 
         if directory:
-            self.d.set_URL(self.url_field.get())
-            self.d.parse_all_imgs()
+            downloader.set_export_directory(directory)
+            downloader.download_album(self.url_field.get())
 
-            answer = askokcancel("Download Confirm",
-                                 "There are "+str(self.d.get_img_num())+" images.")
-            if answer:
-                self.__download(directory)
+            # answer = askokcancel("Download Confirm",
+            #                      "There are "+str(self.d.get_img_num())+" images.")
+            # if answer:
+            #     self.__download(directory)
 
-    def __download(self, path):
-        self.d.set_export_directory(path)
-        self.d.download_all_img()
-
-        if self.d.is_success_download:
-            msg = "Success"
-        else:
-            msg = "The following images are not failed to download.\n"
-            for fail_img in self.d.get_fail_imgs():
-                msg += fail_img["name"]+"\t"+fail_img["url"]+"\n"
-
-        self.msg_label['text'] = msg
-        self.msg_label.grid(row=2, column=0)
+        self.msg_label.text = "Successfully Downloaded"
 
 
 if __name__ == '__main__':

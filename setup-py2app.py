bc8ac68cacb96d2afa444bf584bc75043ad45047
LeeW <cl87654321@gmail.com>
2015-09-28 01:09:24 +0800

    Update setup-py2app for handling SSLError

--- setup-py2app.py
+++ setup-py2app.py
@@ -13,14 +13,19 @@ build_exe_options = {"includes": includes,
                      "packages": packages,
                      "include_files": include_files}
 
-setup(app=[main_python_file],
-      setup_requires=["py2app"],
-      name=application_title,
-      version="1.0",
-      description=application_description,
-      author="LeeW",
-      author_email="cl87654321@gmail.com",
-      url="https://github.com/Lee-W/repo",
-      options={"build_exe":  build_exe_options},
-      )
-
+setup(
+    app=[main_python_file],
+    setup_requires=["py2app"],
+    name=application_title,
+    version="1.0",
+    description=application_description,
+    author="LeeW",
+    author_email="cl87654321@gmail.com",
+    url="https://github.com/Lee-W/repo",
+    options={
+        "build_exe":  build_exe_options,
+        'py2app': {
+          'packages': ['requests']
+        }
+    },
+)

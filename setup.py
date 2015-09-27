944aa41563ad5f832e41105727bf99a49db059f0
LeeW <cl87654321@gmail.com>
2015-09-27 23:50:59 +0800

    Update setup for SSLError handling

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import sys
+import os
 from cx_Freeze import setup, Executable
+import requests.certs
+
+os.environ["REQUESTS_CA_BUNDLE"] = os.path.join(os.getcwd(), "cacert.pem")
 
 main_python_file = "./Downloader_GUI.py"
 application_title = "Flickr Album One Click Download"
@@ -8,7 +12,7 @@ application_description = "Download Flickr album images for a ceratin url"
 includes = []
 excludes = []
 packages = []
-include_files = []
+include_files = [(requests.certs.where(), 'cacert.pem')]
 build_exe_options = {"includes": includes,
                      "excludes": excludes,
                      "packages": packages,
@@ -18,13 +22,13 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(name=application_title,
-      version="1.0",
-      description=application_description,
-      author="LeeW",
-      author_email="cl87654321@gmail.com",
-      url="https://github.com/Lee-W/repo",
-      options={"build_exe":  build_exe_options},
-      executables=[Executable(main_python_file, base=base)]
-      )
-
+setup(
+    name=application_title,
+    version="1.0",
+    description=application_description,
+    author="LeeW",
+    author_email="cl87654321@gmail.com",
+    url="http://lee-w.github.io/Flickr_Album_Downloader/",
+    options={"build_exe":  build_exe_options},
+    executables=[Executable(main_python_file, base=base)]
+)

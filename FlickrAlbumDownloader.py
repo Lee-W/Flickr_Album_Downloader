74f91aa7b2644b55e3d909f7d89ec56baf8a5357
Wei Lee <weilee.rx@gmail.com>
2019-12-31 17:57:35 +0800

    style(FlickrAlbumDownloader): blackify

--- FlickrAlbumDownloader.py
+++ FlickrAlbumDownloader.py
@@ -9,7 +9,7 @@ import flickrapi
 from flickrapi.exceptions import FlickrError
 
 
-class FlickrAlbumDownloader(object):
+class FlickrAlbumDownloader:
     DEFAULT_PATH = "./download_img"
 
     def __init__(self, key, secret):
@@ -37,18 +37,19 @@ class FlickrAlbumDownloader(object):
         self.__get_album_list(album_id)
         photo_num = len(self.album)
         for index, photo in enumerate(self.album):
-            photo_id = photo['id']
-            title = photo['title']
+            photo_id = photo["id"]
+            title = photo["title"]
 
             try:
-                photo_size_info = self._flickr.photos.getSizes(photo_id=photo_id,
-                                                               format='json')
+                photo_size_info = self._flickr.photos.getSizes(
+                    photo_id=photo_id, format="json"
+                )
             except KeyboardInterrupt:
                 print("Terminated by User")
             else:
-                photo_size_info = json.loads(photo_size_info.decode('utf-8'))
+                photo_size_info = json.loads(photo_size_info.decode("utf-8"))
 
-                url = photo_size_info['sizes']['size'][-1]['source']
+                url = photo_size_info["sizes"]["size"][-1]["source"]
                 file_extension = FlickrAlbumDownloader.__match_file_extension(url)
                 full_path = os.path.join(self._path, title + "." + file_extension)
 
@@ -57,11 +58,12 @@ class FlickrAlbumDownloader(object):
                 except FileExistsError:
                     print("%s Exist" % title)
                 else:
-                    print("%d/%d - %s Downloaded \n" %
-                          (index+1, len(self.album), title))
+                    print(
+                        "%d/%d - %s Downloaded \n" % (index + 1, len(self.album), title)
+                    )
 
                     if callback:
-                        callback(title, index+1, photo_num)
+                        callback(title, index + 1, photo_num)
 
     @staticmethod
     def __reporthook(block_num, block_size, total_size):
@@ -69,14 +71,16 @@ class FlickrAlbumDownloader(object):
 
         if total_size > 0:
             try:
-                percent = min((current_progress*100)/total_size, 100)
+                percent = min((current_progress * 100) / total_size, 100)
             except Exception:
                 percent = 100
 
-            s = "\r%5.1f%% %*d/%d" % (percent,
-                                      len(str(total_size)),
-                                      current_progress,
-                                      total_size)
+            s = "\r%5.1f%% %*d/%d" % (
+                percent,
+                len(str(total_size)),
+                current_progress,
+                total_size,
+            )
             sys.stderr.write(s)
             if current_progress > total_size:
                 sys.stderr.write("\n")
@@ -90,7 +94,7 @@ class FlickrAlbumDownloader(object):
 
     @staticmethod
     def __match_file_extension(url):
-        m = re.search("\.([a-zA-Z]*)$", url)
+        m = re.search(r"\.([a-zA-Z]*)$", url)
         return m.groups()[0]
 
     # TODO: option to overwrite duplicate file
@@ -115,7 +119,7 @@ def main():
     API_info = load_API_info(API_path)
 
     try:
-        fad = FlickrAlbumDownloader(API_info['key'], API_info['secret'])
+        fad = FlickrAlbumDownloader(API_info["key"], API_info["secret"])
 
         album_url = input("Please input album url: ")
     except FlickrError:
@@ -127,5 +131,6 @@ def main():
         fad.set_export_directory()
         fad.download_album(album_id)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
